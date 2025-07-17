71bb85f6aff677710ccbc76a1505c68a3d6de9fa
PrabinPyakurel82 <prabinpyakurel82@gmail.com>
2025-07-17 17:05:34 +0545

    Configurations and setup

---
+++ app/core/config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings:
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+    PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+    PINECONE_INDEX = os.getenv("PINECONE_INDEX")
+    DB_URL = os.getenv("DB_URL")
+
+settings = Settings()
---
+++ app/core/models.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from sqlalchemy import Column,Integer,String,DateTime,text
+
+from app.core.database import Base
+
+
+class UploadedFile(Base):
+    __tablename__ = "uploaded_files"
+    id = Column(Integer, primary_key = True, index=True)
+    file_name = Column(String)
+    embedding_model = Column(String)
+    chunking_strategy = Column(String)
+    created_at = Column(DateTime, default=datetime.now)
