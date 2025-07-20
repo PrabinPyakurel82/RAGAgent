9f7e3d85c4d152ad675b6cf10913a3bd43af3764
PrabinPyakurel82 <prabinpyakurel82@gmail.com>
2025-07-20 18:20:01 +0545

    included chunking strategies into one

--- app/chunking/recursive_chunking.py
+++
@@ -1,5 +0,0 @@
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-
-def recursive_chunk(text):
-    splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
-    return splitter.split_text(text)
--- app/chunking/semantic_paragraph_chunking.py
+++
@@ -1,9 +0,0 @@
-from langchain.text_splitter import CharacterTextSplitter
-
-def semantic_paragraph_chunk(text):
-    splitter = CharacterTextSplitter(
-        separator="\n\n", 
-        chunk_size=500,
-        chunk_overlap=50,
-    )
-    return splitter.split_text(text)
